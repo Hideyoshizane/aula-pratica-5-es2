1925a2ce486f3aac137c8dd727d0893c152d9d64
Hideki Yoshizane <hideki.yoshizane04@gmail.com>
2023-06-01 18:18:20 -0300

    tests update

--- tests/test.js
+++ tests/test.js
@@ -6,38 +6,86 @@ const { window } = new JSDOM(html);
 
 const displayResult = window.document.querySelector('input[name="displayResult"]');
 
-// Test addition
 test('Addition', () => {
-  calcNumbers('2');
-  expect(displayResult.value).toBe('2');
-  
+  calcNumbers('7');
+  expect(displayResult.value).toBe('7');
+
   calcNumbers('+');
-  expect(displayResult.value).toBe('2+');
-  
-  calcNumbers('3');
-  expect(displayResult.value).toBe('2+3');
-  
+  expect(displayResult.value).toBe('7+');
+
+  calcNumbers('8');
+  expect(displayResult.value).toBe('7+8');
+
+  displayResult.value = eval(displayResult.value);
+  expect(displayResult.value).toBe('15');
+});
+
+test('Subtraction', () => {
+  calcNumbers('9');
+  expect(displayResult.value).toBe('9');
+
+  calcNumbers('-');
+  expect(displayResult.value).toBe('9-');
+
+  calcNumbers('4');
+  expect(displayResult.value).toBe('9-4');
+
   displayResult.value = eval(displayResult.value);
   expect(displayResult.value).toBe('5');
 });
 
-// Test subtraction
-test('Subtraction', () => {
+test('Multiplication', () => {
+  calcNumbers('2');
+  expect(displayResult.value).toBe('2');
+
+  calcNumbers('*');
+  expect(displayResult.value).toBe('2*');
+
+  calcNumbers('3');
+  expect(displayResult.value).toBe('2*3');
+
+  displayResult.value = eval(displayResult.value);
+  expect(displayResult.value).toBe('6');
+});
+
+test('Division', () => {
   calcNumbers('8');
   expect(displayResult.value).toBe('8');
-  
-  calcNumbers('-');
-  expect(displayResult.value).toBe('8-');
-  
-  calcNumbers('4');
-  expect(displayResult.value).toBe('8-4');
-  
+
+  calcNumbers('/');
+  expect(displayResult.value).toBe('8/');
+
+  calcNumbers('2');
+  expect(displayResult.value).toBe('8/2');
+
   displayResult.value = eval(displayResult.value);
   expect(displayResult.value).toBe('4');
 });
 
-// Add more test cases for multiplication, division, and other operations
+test('Addition with decimal number', () => {
+  calcNumbers('3');
+  expect(displayResult.value).toBe('3');
+
+  calcNumbers('.');
+  expect(displayResult.value).toBe('3.');
+
+  calcNumbers('5');
+  expect(displayResult.value).toBe('3.5');
+
+  calcNumbers('+');
+  expect(displayResult.value).toBe('3.5+');
+
+  calcNumbers('2');
+  expect(displayResult.value).toBe('3.5+2');
+
+  displayResult.value = eval(displayResult.value);
+  expect(displayResult.value).toBe('5.5');
+});
 
 function calcNumbers(result) {
-  displayResult.value = displayResult.value + result;
+  if (result === '=') {
+    displayResult.value = eval(displayResult.value);
+  } else {
+    displayResult.value = displayResult.value + result;
+  }
 }
